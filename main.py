67cbd39407e54773af593916cbf31c7dfcf201e4
ilnur786 <Ilnur-qwerty@mail.ru>
2021-02-28 21:08:47 +0300

    Update fit_function. Add output information in training cycle of ga. Change steps in ga.initialize() and value in training cycle.

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ logger.addHandler(fh)
 logger.info("Program started")
 
 target = next(SDFRead(open('./data/tylenol.sdf', encoding='UTF-8')))
-env = SimpleSynthesis(target, steps=10)
+env = SimpleSynthesis(target, steps=100)
 target_bits = get_feature_bits(target)
 step_per_score = dict()
 
@@ -38,12 +38,18 @@ def fit_func(hromosoma):
     for action in hromosoma:
         state, reward, done, info = env.step(action)
         if done:
-            if state == target_bits:
+            if env.depth < 5 and not env.stop:
                 logger.info(f'синтетический путь для молекулы {target} = {[step for step in env.render()]}')
                 print(f'синтетический путь для молекулы {target} = {[step for step in env.render()]}')
-            elif env.steps > env.max_steps:
-                logger.info(f'Превышено число шагов: {env.steps} > {env.max_steps}')
-                print(f'Превышено число шагов: {env.steps} > {env.max_steps}')
+                return reward
+                #break добавить? чтобы при синтезировании молекулы закончить или в цилке га сравнение симилярити == 1 достаточно?
+        # if done:
+        #     if state == target:
+        #         logger.info(f'синтетический путь для молекулы {target} = {[step for step in env.render()]}')
+        #         print(f'синтетический путь для молекулы {target} = {[step for step in env.render()]}')
+        #     elif env.steps > env.max_steps:
+        #         logger.info(f'Превышено число шагов: {env.steps} > {env.max_steps}')
+        #         print(f'Превышено число шагов: {env.steps} > {env.max_steps}')
             # elif env.depth >= 5:
             #     logger.info(f'Превышена глубина синтеза {env.depth} > 5')
             #     print(f'Превышена глубина синтеза {env.depth} > 5')
@@ -60,14 +66,25 @@ ga.set_crossover_type(two_point_crossover)
 ga.set_mutator_type(uniform_mutation)
 ga.set_fitness(fit_func)
 
-ga.initialize(space=env.action_space, steps=10)
+ga.initialize(space=env.action_space, steps=25)  #длина хромосомы
 # ga.run(n_iter=500, verbose=True)
 
-for i in range(50):
+for i in range(2000):
     ga.step()
-    print(f'ga.score = {ga.best_individual().score}')
-    logger.info(f'{i}: {ga.best_individual().score}')
-    step_per_score[i] = ga.best_individual().score
+    b_individual = ga.best_individual()
+    statistics = ga.population.calc_statistics()
+    print(f'ga.score = {b_individual.score}')
+    logger.info(f'ga_score-{i}: {b_individual.score}')
+    print(f'ga_statistics-{i}: {statistics}')
+    logger.info(f'ga_statistics-{i}: {statistics}')
+    print(f'ga_best_individual-{i}: {b_individual}')
+    logger.info(f'ga_best_individual-{i}: {b_individual}')
+    step_per_score[i] = b_individual.score
+    if b_individual == 1:
+        print('молекула синтезирована!!!!!!!!!!!!!!!!!!!!!!!!')
+        logger.info('молекула синтезирована!!!!!!!!!!!!!!!!!!!!!!!!')
+        break
+    env.reset()
 logger.info('Done')
 
 with open('ga_result.pickle', 'wb') as f:
