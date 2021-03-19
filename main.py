a35aeb55d56488c7517406a73ded19d2664ff018
ilnur786 <Ilnur-qwerty@mail.ru>
2021-03-19 10:43:32 +0300

    ...

--- main.py
+++ main.py
@@ -5,20 +5,19 @@ from genopt.crossovers import two_point_crossover
 from genopt.mutators import uniform_mutation
 import os
 from CGRtools.files import SDFRead
-from pickle import dump
-
+from pickle import dump, load
+import time
 import logging
 
 os.environ['DB'] = './data/db.shelve'
 os.environ['DATA'] = './data/rules'
 
 from RNNSynthesis.environment import SimpleSynthesis
-from RNNSynthesis.helper import get_feature, get_feature_bits
 logger = logging.getLogger("exampleApp")
 logger.setLevel(logging.INFO)
 
 # create the logging file handler
-fh = logging.FileHandler("GA.log")
+fh = logging.FileHandler("GA.log", mode='w')
 
 formatter = logging.Formatter()
 fh.setFormatter(formatter)
@@ -28,67 +27,81 @@ logger.addHandler(fh)
 
 logger.info("Program started")
 
-target = next(SDFRead(open('./data/tylenol.sdf', encoding='UTF-8')))
-env = SimpleSynthesis(target, steps=100)
-target_bits = get_feature_bits(target)
+filename = "tylenol"
+target = next(SDFRead(open(f'./data/{filename}.sdf', encoding='UTF-8')))
+env = SimpleSynthesis(target, steps=10 ** 6)
 step_per_score = dict()
 
 
 def fit_func(hromosoma):
+    env.reset()
+    last_reward = 0
     for action in hromosoma:
         state, reward, done, info = env.step(action)
-        if done:
-            if env.depth < 5 and not env.stop:
-                logger.info(f'синтетический путь для молекулы {target} = {[step for step in env.render()]}')
-                print(f'синтетический путь для молекулы {target} = {[step for step in env.render()]}')
-                return reward
-                #break добавить? чтобы при синтезировании молекулы закончить или в цилке га сравнение симилярити == 1 достаточно?
-        # if done:
-        #     if state == target:
-        #         logger.info(f'синтетический путь для молекулы {target} = {[step for step in env.render()]}')
-        #         print(f'синтетический путь для молекулы {target} = {[step for step in env.render()]}')
-        #     elif env.steps > env.max_steps:
-        #         logger.info(f'Превышено число шагов: {env.steps} > {env.max_steps}')
-        #         print(f'Превышено число шагов: {env.steps} > {env.max_steps}')
-            # elif env.depth >= 5:
-            #     logger.info(f'Превышена глубина синтеза {env.depth} > 5')
-            #     print(f'Превышена глубина синтеза {env.depth} > 5')
-            # elif action == 'STOP':
-            #     logger.info('Выпало действие STOP')
-            #     print('Выпало действие STOP')
-            return reward
+        if state:
+            if len(state) - len(target) >= 20:
+                break
+            last_reward = reward
+            if done:
+                if env.depth < 5 and not env.stop:
+                    logger.info(f'синтетический путь для молекулы {target} = {[step for step in env.render()]}')
+                    print(f'синтетический путь для молекулы {target} = {[step for step in env.render()]}')
+                    break
+                else:
+                    logger.info('done, но молекула не синтезирована')
+                    print('done, но молекула не синтезирована')
+    return last_reward
 
-
-ga = SGA(task='maximize', pop_size=50, cross_prob=0.8, mut_prob=0.2, elitism=True)
+ga = SGA(task='maximize', pop_size=50, cross_prob=0.8, mut_prob=0.2, elitism=True, n_cpu=20)
 ga.set_selector_type(tournament_selection)
 ga.set_scaler_type(sigma_trunc_scaling)
 ga.set_crossover_type(two_point_crossover)
 ga.set_mutator_type(uniform_mutation)
 ga.set_fitness(fit_func)
 
-ga.initialize(space=env.action_space, steps=25)  #длина хромосомы
-# ga.run(n_iter=500, verbose=True)
+ga.initialize(space=env.action_space, steps=10)  #длина хромосомы
 
-for i in range(1000):
+# если хочу продолжить с последней популяции. НЕ ПЕРЕПУТАТЬ ДЛЯ РАЗНЫХ ТАРГЕТОВ
+with open(f'ga_last_population_{filename}.pickle', 'rb') as f:
+    last_pop = load(f)
+
+ga.population = last_pop
+ga.step()
+#
+
+for i in range(100000):
+    start = time.time()
     ga.step()
+    end = time.time()
+    print(end - start)
     b_individual = ga.best_individual()
+    score = b_individual.score
     statistics = ga.population.calc_statistics()
+    step_per_score[i] = b_individual.score
 
-    print(f'ga.score = {b_individual.score}')
-    logger.info(f'ga_score-{i}: {b_individual.score}')
+    with open('ga_result.pickle', 'wb') as f:
+        dump(step_per_score, f)
+
+    print(f'ga.score = {score}')
+    logger.info(f'ga_score-{i}: {score}')
 
     print(f'ga_best_individual-{i}: {b_individual}')
     logger.info(f'ga_best_individual-{i}: {b_individual}')
 
-    logger.info(f'ga_statistics-{i}: {statistics}')   # зачем эта статистика нужна?
+    logger.info(f'ga_statistics-{i}: {statistics.stats}')   # будут различные параметры в виде словаря. можно добавить туда свой ключ с лучшей хромосомой на кажом шаге
 
-    step_per_score[i] = b_individual.score
-    if b_individual == 1:
+    last_pop = ga.population
+
+    with open(f'ga_last_population_{filename}.pickle', 'wb') as f:
+        dump(last_pop, f)
+
+
+    if b_individual.score >= 10:
         print('молекула синтезирована!!!!!!!!!!!!!!!!!!!!!!!!')
         logger.info('молекула синтезирована!!!!!!!!!!!!!!!!!!!!!!!!')
         break
-    env.reset()
+
+
 logger.info('Done')
 
-with open('ga_result.pickle', 'wb') as f:
-    dump(step_per_score, f)
+
