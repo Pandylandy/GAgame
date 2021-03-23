ba53357e6142ade0cec07f429696c1767c5a616f
ilnur786 <Ilnur-qwerty@mail.ru>
2021-03-23 15:39:40 +0300

    Add graphic plot from Tanimoto

--- genetic_graphic.py
+++ genetic_graphic.py
@@ -14,17 +14,32 @@ for i in count:
     with open(f'score_per_step/{filename}/{i}.pickle', 'rb') as f:
         tmp = load(f)
     result.update(tmp)
-# with open(f'ga_result.pickle', 'rb') as f:
-#     result = load(f)
-#
-# d = {'ga_score': [value for key, value in result.items()]}
 
+def reward_to_tanimoto(reward):
+    return (1 + 10 ** (-10)) - (10 ** (0 - reward))
+
+#график с reward
 df = pd.DataFrame.from_dict(result, orient='index', columns=['ga_score'])
 
 fig, ax = plt.subplots(figsize=(15, 7))
-plt.title(filename)
+plt.title(f'Reward for {filename}')
 plt.plot(df)
 plt.xlabel('steps')
 plt.ylabel('ga_score')
-plt.savefig(f'graphics/training_result_{filename}.png')
-plt.show()
+plt.savefig(f'graphics/reward_{filename}.png')
+# plt.show()
+
+# график с танимомто
+result_tanimoto = dict()
+for key, value in result.items():
+    result_tanimoto[key] = reward_to_tanimoto(value)
+
+df = pd.DataFrame.from_dict(result_tanimoto, orient='index', columns=['ga_score'])
+
+fig, ax = plt.subplots(figsize=(15, 7))
+plt.title(f'Tanimoto for {filename}')
+plt.plot(df)
+plt.xlabel('steps')
+plt.ylabel('Tanimoto')
+plt.savefig(f'graphics/tanimoto_{filename}.png')
+# plt.show()
